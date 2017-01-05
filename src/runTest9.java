cc78d1967af0453c6913989515fcba75dd52b874
ubrals <ubrals@192.168.0.104>
2017-01-04 23:40:11 -0200

    Teste de unicode simples

--- src/runTest9.java
+++ src/runTest9.java
@@ -17,6 +17,16 @@ public class runTest9 {
             System.out.print((char)Math.abs(b));
         System.out.println();
         System.out.println(transaction0.toMakeHash());
+        
+        for (int i=0x2500;i<=0x257F;i++)
+        {
+            System.out.printf("%d=0x%x : %c\n",i, i,(char)i);
+        }
+        for (int i=0x0000;i<=0x1111;i++)
+        {
+            System.out.printf("%d=0x%x : %c\n",i, i,(char)i);
+        }
+
     }
 
 }

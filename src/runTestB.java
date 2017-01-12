6b28b1b6f0e6b433fc2bad00522d52f82ba29cae
ubrals <ubrals@192.168.0.104>
2017-01-11 23:42:13 -0200

    PN:ConversionsNew; hexToDec

--- src/runTestB.java
+++ src/runTestB.java
@@ -3,8 +3,25 @@ import static utils.ConversionsNew.*;
 public class runTestB {
 
     public static void main(String[] args) {
+        byte[] reverse;
+        reverse = decToByteArrayEven(1483939849l);
+        //49525651575157565257
+        reverse = decToByteArrayEvenInternalByteOrder(1483939849l);
+        //57525657515751565249
+        
+        reverse = decToByteArrayEven(148393984l);
+        //48495256515751575652
+        reverse = decToByteArrayEvenInternalByteOrder(148393984l);
+        //52565751575156524948
+        
         System.out.println(decToHex(89999l));
         System.out.println(decToHexInternalByteOrder(89999l));
+        
+        for(int i=0; i<reverse.length; i++)
+            System.out.print(reverse[i]);
+        
+        System.out.println(hexToDec("015f8f"));
+        System.out.println(hexToDec("8f5f01"));
     }
 
 }
--- src/utils/ConversionsNew.java
+++ src/utils/ConversionsNew.java
@@ -10,6 +10,16 @@ public class ConversionsNew {
         
         return swap;
     }
+    public static long hexToDec(String num){
+        long ret;
+        if(num.charAt(0) == '0')
+            ret = Long.valueOf(num.substring(1, num.length()));
+        else
+            ret = Long.parseUnsignedLong("0x"+ num, 16);
+        
+        return ret;
+    }
+
     
     public static String decToHexInternalByteOrder(long num){
         String swap = decToHex(num);
@@ -30,7 +40,6 @@ public class ConversionsNew {
     
     public static String decToStringEven(long num){
         String swap = decToString(num);
-        
         if(swap.length()%2 != 0){
             swap = "0" + swap;
         }
@@ -42,28 +51,54 @@ public class ConversionsNew {
         String swap = decToStringEven(num);
         byte[] retArray = new byte[swap.length()];
         
-        for(int i=0; i<swap.length()-1; i++){
+//        System.out.println("num:"+ num + " swp:"+swap);
+        for(int i=0; i<swap.length(); i++){
+//            System.out.println("swp:chr:"+i+ ":"+swap.charAt(i) +" ");
             retArray[i] = (byte) swap.charAt(i);
         }
+//        System.out.println();
         
         return retArray;
     }
     
-    public static byte[] decToByteArrayEvenInternalByteORder(long num){
-        byte[] retArray;
+    public static byte[] decToByteArrayEvenInternalByteOrder(long num){
         int db=0;
         String dch = "";
         byte[] swap = decToByteArrayEven(num);
-        
-        for(int i=0; i<swap.length()-1; i=i+2){
-            retArray = String.valueOf(swap.charAt(i)) + String.valueOf(swap.charAt(i+1)) + retArray;
+        byte[] retArray = new byte[swap.length];
+
+        for(int i=0; i<swap.length-1; i=i+2){
+//        for(int i=0; i<swap.length-1; i=i+2){
+            retArray[swap.length-i-2] = swap[i+1];
+            retArray[swap.length-i-1] = swap[i];
+//            + retArray;
         }
         
         return retArray;
     }
     
-    public static byte[] swapInternalByteOrder(byte[] bytes){
+//    public static byte[] swapInternalByteOrder(byte[] bytes){
+//        
+//    }
+    public static String right(String word, int count){
+        String ret = "";
+        int len = word.length();
+        for(int i=0; i<count; i++){
+            ret = String.valueOf(word.charAt(len-1-i)) + ret;
+        }
         
+        return ret;
     }
+    
+    public static String left(String word, int count){
+        String ret = "";
+        for(int i=0; i<count; i++){
+            ret = ret + String.valueOf(word.charAt(i));
+        }
+        
+        return ret;
+    }
+    
+
 
 }
